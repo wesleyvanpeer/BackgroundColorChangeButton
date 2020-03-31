18233a0e76c04e8fdb69f549422d0f8bb7b7570c
Wesley van Peer - DSV <wesley.vanpeer@NL.DSV.COM>
2020-03-31 12:52:09 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//Choose a random color
+const button = document.querySelector('button')
+const body = document.querySelector('body')
+const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+
+button.addEventListener('click', changeBackground)
+
+function changeBackground(){
+const colorIndex= parseInt(Math.random()*colors.length)
+body.style.backgroundColor = colors[colorIndex]
+}
